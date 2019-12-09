4a622a3aec152e4f020e8c1415308de8061ae9d4
Peyton Turner <donaldpeytonturner@gmail.com>
2019-12-08 20:46:10 -0800

    Drops unused boilerplate from initial setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-// Some content Copyright 2018 Serde Developers
-//
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
@@ -8,18 +6,9 @@
 #![allow(dead_code, unused_imports, unused_variables)]
 pub mod ion_types;
 pub mod parser;
-//mod error;
-//mod ser;
 
 extern crate bit_vec;
 extern crate serde;
 
 #[macro_use]
 extern crate num_derive;
-
-// #[cfg(test)]
-// #[macro_use]
-// extern crate serde_derive;
-
-//pub use error::{Error, Result};
-//pub use ser::{to_string, to_bytes};
