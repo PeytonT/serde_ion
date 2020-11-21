619d1500abeeaf2fdc3da3fdadb4a0bb88ea94f7
Peyton Turner <donaldpeytonturner@gmail.com>
2020-11-21 13:46:09 -0800

    Removes unused BVM constant definitions

--- src/parser/parse.rs
+++ src/parser/parse.rs
@@ -9,66 +9,30 @@ use crate::{
     },
     value::Value,
 };
-use nom::{
-    bytes::complete::{tag, take},
-    sequence::tuple,
-    Err,
+use nom::{bytes::complete::tag, Err};
+
+const BVM_START_BYTE: u8 = 0xE0;
+const BVM_END_BYTE: u8 = 0xEA;
+
+#[derive(Clone, Debug, PartialEq)]
+struct IonVersion {
+    major: u8,
+    minor: u8,
+}
+const ION_VERSION_1_0: IonVersion = IonVersion {
+    major: 0x01,
+    minor: 0x00,
 };
 
 // Binary Ion streams begin with a four-octet Binary Version Marker
 // BVM_START MAJOR_VERSION MINOR_VERSION BVM_END
 // For version 1.0, this is 0xE0 0x01 0x00 0xEA
-pub const BVM_BYTES: usize = 4;
-pub const BVM_START_BYTE: u8 = 0xE0;
-pub const BVM_END_BYTE: u8 = 0xEA;
-pub const BVM_START: [u8; 1] = [BVM_START_BYTE];
-pub const BVM_END: [u8; 1] = [BVM_END_BYTE];
-pub const VERSION_1_0: IonVersion = IonVersion {
-    major: 0x01,
-    minor: 0x00,
-};
-pub const BVM_1_0: [u8; 4] = ion_bvm(VERSION_1_0.major, VERSION_1_0.minor).as_bytes();
-
-#[derive(Clone, Debug, PartialEq)]
-pub struct IonVersion {
-    major: u8,
-    minor: u8,
-}
-
-#[derive(Clone, Debug, PartialEq)]
-struct BinaryVersionMarker {
-    start: u8,
-    major: u8,
-    minor: u8,
-    end: u8,
-}
-
-const fn ion_bvm(major_version: u8, minor_version: u8) -> BinaryVersionMarker {
-    BinaryVersionMarker {
-        start: BVM_START_BYTE,
-        major: major_version,
-        minor: minor_version,
-        end: BVM_END_BYTE,
-    }
-}
-
-impl BinaryVersionMarker {
-    const fn as_bytes(&self) -> [u8; 4] {
-        [self.start, self.major, self.minor, self.end]
-    }
-}
-
-fn take_ion_version(input: &[u8]) -> IonResult<&[u8], IonVersion> {
-    let (input, (start, major, minor, end)) =
-        tuple((tag(BVM_START), take(1usize), take(1usize), tag(BVM_END)))(input)?;
-    Ok((
-        input,
-        IonVersion {
-            major: major[0],
-            minor: minor[0],
-        },
-    ))
-}
+pub const BVM_1_0: [u8; 4] = [
+    BVM_START_BYTE,
+    ION_VERSION_1_0.major,
+    ION_VERSION_1_0.minor,
+    BVM_END_BYTE,
+];
 
 fn parse(input: &[u8]) -> IonResult<&[u8], Vec<Value>> {
     all_consuming(map(many0(preceded(tag(BVM_1_0), parse_ion_1_0())), |x| {
@@ -123,15 +87,6 @@ mod tests {
     fn binary_version_marker_test() {
         let data = include_bytes!("../../tests/ion-tests/iontestdata/good/null.10n");
         assert_eq!(&BVM_1_0, &data[0..4]);
-    }
-
-    #[test]
-    fn take_binary_version_marker_test() {
-        let null_null = include_bytes!("../../tests/ion-tests/iontestdata/good/null.10n");
-        assert_eq!(
-            take_ion_version(null_null),
-            Ok((&null_null[4..], VERSION_1_0))
-        );
     }
 
     // Parse null tests
